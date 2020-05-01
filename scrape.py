dd11c69fa763d2363de2f14a50d7ddeea23f971f
Stilghar <plopaciuk@gmail.com>
2020-05-01 14:07:00 +0200

    Update scrape.py
    Some cleanup and variables renaming

--- scrape.py
+++ scrape.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 def scrape(page_url, output):
-    result=[]
+    scrape_result=[]
+
     page = get(page_url)
-    
     soup = BeautifulSoup(page.content, 'html.parser')
 
     for item in soup.find_all('div', class_='caption'):
@@ -23,14 +23,15 @@ def scrape(page_url, output):
             except:
                 pass
 
-        result.append([price[1:], name])
+        scrape_result.append([price[1:], name])
 
     now = datetime.now()
 
-    with open(str(now.year) + str(now.month) + str(now.day) + \
-              str(now.hour) + str(now.minute) + str(now.second) + \
-              ' ' + output, 'w') as lista_txt:
+    timestamp = str(now.year)[2:] + str(now.month).zfill(2) + str(now.day).zfill(2) + '_' + \
+                str(now.hour).zfill(2) + str(now.minute).zfill(2) + str(now.second).zfill(2)
 
-        for item in result:
-            lista_txt.write(','.join(item)+'\n')
+    with open(timestamp + '_' + output, 'w') as output_file:
+        for item in scrape_result:
+            output_file.write(','.join(item)+'\n')
+
     return 0
